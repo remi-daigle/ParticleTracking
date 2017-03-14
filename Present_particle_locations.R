368bd336c3adb3e3a1808a2a8848c1a710ec2b8a
Christopher-Blackford <christopher.blackford@mail.utoronto.ca>
2017-03-14 12:39:20 -0400

    Got the new study extent working well

--- Present_particle_locations.R
+++ Present_particle_locations.R
@@ -135,30 +135,28 @@ write.csv(Con_df, "./output/connectivity_tables/Con_df.csv", row.names = F)
 
 
 #Clipping to your study extent
-Ecozones <- readOGR("./cuke_present/StudyExtent/BC_Ecozones", "Ecozones_BC")
+Ecozone_mask <- readOGR("./cuke_present/StudyExtent/BC_EcozonesMask", "Ecozone_mask")
 #Need to remove offshore ecoregion - this is messy
-Ecozones@data$Clipping <- c(1,2,99,3)
-Ecozones@data
-Ecozones <- Ecozones[Ecozones$Clipping < 99,]
-plot(Ecozones)
+Ecozone_mask@data$Clipping <- c(1,2,99,3)
+Ecozone_mask@data
+Ecozone_mask <- Ecozone_mask[Ecozone_mask$Clipping < 99,]
+plot(Ecozone_mask)
 
 #Loading Remi's grid where larvae were released
 grid <- readOGR("./cuke_present/StudyExtent/Starting_grid", "grid")
 #Dissolve into one polygon since so you can change grid dimensions
-ConPoly <- spTransform(grid, Ecozones@proj4string) #use your custom BC projection for this
+ConPoly <- spTransform(grid, Ecozone_mask@proj4string) #use your custom BC projection for this
 ConPoly <- gUnaryUnion(ConPoly)
 plot(ConPoly)
 
-#Adding dataframe to convert back to SpatialPolygonsDataFrame - if need be....
-ConPoly_ID <- row.names(ConPoly)
-ConPoly_ID <- as.data.frame(ConPoly_ID)
-# And add the data back in
-ConPoly <- SpatialPolygonsDataFrame(ConPoly, ConPoly_ID)
-rm(ConPoly_ID)
+#Intersecting
+Study_extent <- gIntersection(ConPoly, Ecozone_mask, byid = FALSE, drop_lower_td = TRUE) #This works, but you'll have to choose a shapefile that includes islands and doesn't cut-off at rivers 
 
-
-Ecozones_new <- gIntersection(ConPoly, Ecozones, byid = FALSE, drop_lower_td = TRUE) #This works, but you'll have to choose a shapefile that includes islands and doesn't cut-off at rivers 
-plot(Ecozones_new)
+#Adding dataframe so you can create a shapefile of new study extent
+Study_extent_ID <- 1 
+Study_extent <- SpatialPolygonsDataFrame(Study_extent, as.data.frame(Study_extent_ID))
+Study_extent@data
+rm(Study_extent_ID)
 
 
 
@@ -216,6 +214,15 @@ Release_df <- point.in.poly(release_larvae, ConPoly)
 
 
 #####################extra TO BE REINSERTED
+
+
+#Adding dataframe to convert back to SpatialPolygonsDataFrame - if need be....
+ConPoly_ID <- row.names(ConPoly)
+ConPoly_ID <- as.data.frame(ConPoly_ID)
+# And add the data back in
+ConPoly <- SpatialPolygonsDataFrame(ConPoly, ConPoly_ID)
+rm(ConPoly_ID)
+
 
 
 ############################################################
